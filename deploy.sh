511dc1c94ebc2aaaa2990da23ee2aa823a3b844c
Federico Zappone <federico.zappone@justanother.cloud>
2026-03-29 04:31:11 +0200

    feat: complete minimal working example

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,18 @@
 script_dir=$(dirname "$0")
 master_compose_file="$script_dir/docker-compose.master.yaml"
 replica_compose_file="$script_dir/docker-compose.replica.yaml"
+env_file="$script_dir/.env"
+env_template_file="$script_dir/.env.example"
 
 mode=${1:-"start"}
+
+if [[ ! -f "$env_file" ]]; then
+    echo "ENV file not found. Generating at $env_file"
+    cp "$env_template_file" "$env_file"
+    local_net_ip=$(ip addr show | grep "192" | awk '{print $2}' | cut -d/ -f1)
+    sed -i "s/=your_local_ip/=$local_net_ip/g" "$env_file"
+fi
+
 
 if [[ "$mode" == "start" ]]; then
     echo "Starting containers..."
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
+from datetime import datetime
 from time import sleep
 
 from dotenv import load_dotenv
 from redis import Redis
-from redis.exceptions import ConnectionError as RedisConnectionError
-from redis.sentinel import MasterNotFoundError, Sentinel
-
-MASTER_SERVICE_NAME = "redis_ha"
-TIMEOUT = 0.5
+from redis.sentinel import Sentinel
 
 load_dotenv()
 
+TIMEOUT = 0.5
+
+master_service_name = os.getenv("MASTER_SERVICE_NAME")
 sentinel_first_ip = os.getenv("SENTINEL_FIRST_IP")
 sentinel_first_port = os.getenv("SENTINEL_FIRST_PORT")
 
@@ -41,28 +41,24 @@ def get_value(client: Redis, key: str) -> str:
 def check_multiple_clients_value(clients: list[Redis]) -> None:
     for client in clients:
         value = get_value(client, test_key)
-        print(f"Value from {client}: {value}")
+        client_address = sentinel.discover_master(master_service_name)
+        print(f"Value from {client_address[0]}:{client_address[1]}: {value}")
 
-def check() -> bool:
-    try:
-        master_address = sentinel.discover_master(MASTER_SERVICE_NAME)
-    except (MasterNotFoundError, RedisConnectionError) as exc:
-        print(f"Sentinel not ready for '{MASTER_SERVICE_NAME}': {exc}")
-        return False
-
+def check() -> None:
+    master_address = sentinel.discover_master(master_service_name)
     print(f"Discovered master via Sentinel: {master_address[0]}:{master_address[1]}")
-    master = sentinel.master_for(MASTER_SERVICE_NAME, socket_timeout=TIMEOUT)
-    replica = sentinel.slave_for(MASTER_SERVICE_NAME, socket_timeout=TIMEOUT)
-    master.set(test_key, test_value)
+    master = sentinel.master_for(master_service_name, socket_timeout=TIMEOUT)
+    replica = sentinel.slave_for(master_service_name, socket_timeout=TIMEOUT)
+    now = datetime.now()
+    master.set(test_key, f"{test_value}_{now.isoformat()}")
     check_multiple_clients_value([master, replica])
-    return True
 
 
 while True:
     print("\nChecking...")
     try:
         check()
-    except (MasterNotFoundError, RedisConnectionError) as exc:
-        print(f"Sentinel not ready for '{MASTER_SERVICE_NAME}': {exc}")
+    except Exception as e:
+        print(f"Sentinel not ready for '{master_service_name}': {e}")
     sleep(1)
 
